9640b49c49730a182c8250a97d689eac03229f8f
Matheus Pimenta <matheuscscp@gmail.com>
2016-07-16 22:08:34 -0300

    Updating usagemode and removing rejudge option

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,35 @@
 #include "global.h"
 
+using namespace std;
+
+static char* exe;
 static void usagemode() {
   printf(
-    "Usage mode:\n"
-    "  install <new directory name without trailing slash>\n"
-    "  start <port>\n"
+    "Usage mode: %s [options...]\n"
+    "\n"
+    "Options:\n"
+    "  install  <new directory name without trailing slash>\n"
+    "  start    <TCP port number>\n"
     "  stop\n"
-    "  rejudge <attempt id> <verdict number>\n"
-    "Verdict numbers:\n"
-    "  AC  = %d\n"
-    "  CE  = %d\n"
-    "  RTE = %d\n"
-    "  TLE = %d\n"
-    "  WA  = %d\n"
-    "  PE  = %d\n",
-    AC, CE, RTE, TLE, WA, PE
+    "  gp       <list of problems by capital letter>\n"
+    "  ga       <(possibly empty) list of problems by capital letter>\n"
+    "  rid      <attempt id>\n"
+    "  rp       <list of problems by capital letter>\n"
+    "  ra       <(possibly empty) list of problems by capital letter>\n"
+    "\n"
+    "gp:  Generate expected outputs for a set of problems.\n"
+    "ga   Generate expected outputs for ALL problems, except for a set.\n"
+    "rid: Re-run attempt by id.\n"
+    "rp:  Re-run ALL attempts for a set of problems.\n"
+    "ra:  Re-run ALL attempts, except for a set of problems.\n",
+    exe
   );
 }
 
 int main(int argc, char** argv) {
+  exe = argv[0];
   if (argc == 1) { usagemode(); return 0; }
-  std::string mode = argv[1];
+  string mode = argv[1];
   if (mode == "install") {
     if (argc == 2) { usagemode(); return 0; }
     Global::install(argv[2]);
@@ -31,10 +40,6 @@ int main(int argc, char** argv) {
   }
   else if (mode == "stop") {
     Global::stop();
-  }
-  else if (mode == "rejudge") {
-    if (argc < 4) { usagemode(); return 0; }
-    Global::rejudge(to<int>(argv[2]), (char)to<int>(argv[3]));
   }
   else usagemode();
   return 0;
