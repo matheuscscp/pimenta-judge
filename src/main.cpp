8d259b9e8d4e3206747ff2a6d4e7b855dca0fa37
Matheus Pimenta <matheuscscp@gmail.com>
2016-08-11 00:51:09 -0300

    Update usage mode

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,23 @@ static void usagemode() {
   printf(
     "Usage mode: %s <option>\n"
     "\n"
-    "Options:\n"
-    "  install <new directory name without trailing slash>\n"
+    "Installation option:\n"
+    "  install <non-existing directory path>\n"
+    "\n"
+    "The following options must be executed inside a directory created with\n"
+    "the 'install' option.\n"
+    "\n"
+    "Offline options:\n"
     "  start\n"
+    "\n"
+    "Online options:\n"
     "  stop\n"
-    "  restart (just a stop followed by a start)\n"
-    "  reload (reload all settings except webserver)\n"
-    "  rerun-id <attempt id>\n"
-    "  rerun-atts <list of problem names>\n"
-    "  rerun-all-atts [list of problem names to skip]\n",
-    exe
+    "  restart (stop followed by start)\n"
+    "  reload (just reload all settings except webserver's)\n"
+    "  rerun-att <attempt id>\n"
+    "  rerun-probs <list of problem id ranges> (* reruns ALL attempts)\n"
+    "    Example to rerun problems 1, 3, 4 and 5: %s rerun-probs 1 3-5\n",
+    exe,exe
   );
 }
 
