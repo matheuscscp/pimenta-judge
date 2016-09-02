abcb755bb8de21a5aba001b3687489c0280a1969
Matheus Pimenta <matheuscscp@gmail.com>
2016-09-02 01:20:09 -0300

    Now the API is half public

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -52,20 +52,14 @@ class Session : public HTTP::Session {
 class Handler : public HTTP::Handler {
 public:
 
-Session& castsess() { return *(Session*)session(); }
-
-void index(const vector<string>& segments = vector<string>()) {
-  string fn = move(HTTP::path(segments,"www"));
-  if (fn != "") file(fn);
-  else not_found();
-}
+int uid() { return session() ? ((Session*)session())->uid : 0; }
 
 Handler() {
 
 route("/",[=](const vector<string>& segments) {
-  index(
-    session() || segments.size() > 0 ? segments : vector<string>{"login.html"}
-  );
+  string fn = move(HTTP::path(segments,"www"));
+  if (fn != "") file(fn);
+  else not_found();
 });
 
 // =============================================================================
@@ -75,9 +69,9 @@ route("/",[=](const vector<string>& segments) {
 route("/status",[=](const vector<string>&) {
   json(map<string,JSON>{
     {"time" , time(nullptr)},
-    {"name" , User::get(castsess().uid)["name"]}
+    {"name" , session() ? User::get(uid())["name"].str() : ""}
   });
-},true);
+});
 
 route("/contests",[=](const vector<string>& args) {
   if (args.size() < 2) { json(Contest::page()); return; }
@@ -87,97 +81,97 @@ route("/contests",[=](const vector<string>& args) {
     return;
   }
   json(Contest::page(page,page_size));
-},true);
+});
 
 route("/problems",[=](const vector<string>& args) {
-  if (args.size() < 2) { json(Problem::page(castsess().uid)); return; }
+  if (args.size() < 2) { json(Problem::page(uid())); return; }
   unsigned page, page_size;
   if (!read(args[0],page) || !read(args[1],page_size)) {
-    json(Problem::page(castsess().uid));
+    json(Problem::page(uid()));
     return;
   }
-  json(Problem::page(castsess().uid,page,page_size));
-},true);
+  json(Problem::page(uid(),page,page_size));
+});
 
 route("/attempts",[=](const vector<string>& args) {
-  if (args.size() < 2) { json(Attempt::page(castsess().uid)); return; }
+  if (args.size() < 2) { json(Attempt::page(uid())); return; }
   unsigned page, page_size;
   if (!read(args[0],page) || !read(args[1],page_size)) {
-    json(Attempt::page(castsess().uid));
+    json(Attempt::page(uid()));
     return;
   }
-  json(Attempt::page(castsess().uid,page,page_size));
-},true);
+  json(Attempt::page(uid(),page,page_size));
+});
 
 route("/users",[=](const vector<string>& args) {
-  if (args.size() < 2) { json(User::page(castsess().uid)); return; }
+  if (args.size() < 2) { json(User::page(uid())); return; }
   unsigned page, page_size;
   if (!read(args[0],page) || !read(args[1],page_size)) {
-    json(User::page(castsess().uid));
+    json(User::page(uid()));
     return;
   }
-  json(User::page(castsess().uid,page,page_size));
-},true);
+  json(User::page(uid(),page,page_size));
+});
 
 route("/logout",[=](const vector<string>&) {
   session(nullptr);
   location("/");
-},true);
+});
 
 route("/contest",[=](const vector<string>& args) {
   int cid;
   if (!read(args[0],cid)) { not_found(); return; }
-  json(Contest::get(cid,castsess().uid));
-},true,false,1);
+  json(Contest::get(cid,uid()));
+},false,false,1);
 
 route("/contest/problems",[=](const vector<string>& args) {
   int cid;
   if (!read(args[0],cid)) { not_found(); return; }
-  json(Contest::get_problems(cid,castsess().uid));
-},true,false,1);
+  json(Contest::get_problems(cid,uid()));
+},false,false,1);
 
 route("/contest/attempts",[=](const vector<string>& args) {
   int cid;
   if (!read(args[0],cid)) { not_found(); return; }
-  json(Contest::get_attempts(cid,castsess().uid));
-},true,false,1);
+  json(Contest::get_attempts(cid,uid()));
+},false,false,1);
 
 route("/contest/scoreboard",[=](const vector<string>& args) {
   int cid;
   if (!read(args[0],cid)) { not_found(); return; }
-  json(Contest::scoreboard(cid,castsess().uid));
-},true,false,1);
+  json(Contest::scoreboard(cid,uid()));
+},false,false,1);
 
 route("/problem",[=](const vector<string>& args) {
   int probid;
   if (!read(args[0],probid)) { not_found(); return; }
-  json(Problem::get(probid,castsess().uid));
-},true,false,1);
+  json(Problem::get(probid,uid()));
+},false,false,1);
 
 route("/problem/statement",[=](const vector<string>& args) {
   int probid;
   if (!read(args[0],probid)) { not_found(); return; }
-  string fn = Problem::statement(probid,castsess().uid);
+  string fn = Problem::statement(probid,uid());
   if (fn != "") file(fn);
-},true,false,1);
+},false,false,1);
 
 route("/attempt",[=](const vector<string>& args) {
   int id;
   if (!read(args[0],id)) { not_found(); return; }
-  json(Attempt::get(id,castsess().uid));
+  json(Attempt::get(id,uid()));
 },true,false,1);
 
 route("/user",[=](const vector<string>& args) {
   int id;
   if (!read(args[0],id)) { not_found(); return; }
-  if (args.size() < 3) { json(User::profile(id,castsess().uid)); return; }
+  if (args.size() < 3) { json(User::profile(id,uid())); return; }
   unsigned page, page_size;
   if (!read(args[1],page) || !read(args[2],page_size)) {
-    json(User::profile(id,castsess().uid));
+    json(User::profile(id,uid()));
     return;
   }
-  json(User::profile(id,castsess().uid,page,page_size));
-},true,false,1);
+  json(User::profile(id,uid(),page,page_size));
+},false,false,1);
 
 // =============================================================================
 // POST
@@ -207,7 +201,7 @@ route("/new_attempt",[=](const vector<string>& args) {
   if (!read(args[0],probid)) { not_found(); return; }
   payload().push_back('\n');
   response(Attempt::create(move(JSON(map<string,JSON>{
-    {"user"     , castsess().uid},
+    {"user"     , uid()},
     {"problem"  , probid},
     {"language" , args[1]},
     {"when"     , when()},
